39ce061e1ca1301ff6805655d76a895413a30131
arunkumararali05 <araliarunkumar@gmail.com>
2024-05-01 12:42:08 +0530

    using do-while loop

---
+++ number_guessing_game.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int numberToGuess, guess, attempts = 0;
+
+    srand(time(0));
+
+
+    numberToGuess = rand() % 100 + 1;
+
+    do {
+        printf("Guess the number (between 1 and 100): ");
+        scanf("%d", &guess);
+
+
+        attempts++;
+
+        if (guess == numberToGuess) {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+        } else if (guess < numberToGuess) {
+            printf("Too low. Try again.\n");
+        } else {
+            printf("Too high. Try again.\n");
+        }
+    } while (guess != numberToGuess);
+
+    return 0;
+}
